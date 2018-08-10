fd0e950e8b3467cc06cf2a29523fa9e66e93c552
Gladys Um <Gladys@Frances-MacBook-Pro.local>
2018-08-09 21:34:42 -0400

    log challenge algo

---
+++ JAVASCRIPT/logChallenge.js
@@ -0,0 +1,6 @@
+export default n => {
+  if (n < 11) return 1;
+  if (Math.ceil(Math.log10(2 * n)) === Math.ceil(Math.log10(n)))
+    return 10 ** Math.floor(Math.log10(n)) / 2;
+  return 10 ** (Math.ceil(Math.log10(n)) - 1);
+};
