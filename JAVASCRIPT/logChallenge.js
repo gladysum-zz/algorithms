0c0b12110e4ff9f396b06e3f2c2a5bc60872f2f6
Gladys Um <Gladys@Frances-MacBook-Pro.local>
2018-08-09 21:44:36 -0400

    comment

--- JAVASCRIPT/logChallenge.js
+++ JAVASCRIPT/logChallenge.js
@@ -1,7 +1,7 @@
 // Assume n is a positive integer
-export default n => {
+function logChallenge(n) {
   if (n < 11) return 1;
   if (Math.ceil(Math.log10(n)) === Math.ceil(Math.log10(2 * n)))
     return 10 ** Math.floor(Math.log10(n)) / 2;
   return 10 ** (Math.ceil(Math.log10(n)) - 1);
-};
+}
